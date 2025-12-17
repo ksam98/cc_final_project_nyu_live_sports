5c4dd4ea125b91205b935ef88303fec129a01727
Samprith Kalakata <ksamprith@gmail.com>
2025-12-17 10:33:12 -0800

    Expose AWS env vars in next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
     unoptimized: true,
   },
   transpilePackages: ['@aws-sdk/client-ivs', '@aws-sdk/client-dynamodb', '@aws-sdk/lib-dynamodb'],
+  env: {
+    APP_AWS_REGION: process.env.APP_AWS_REGION,
+    APP_AWS_ACCESS_KEY_ID: process.env.APP_AWS_ACCESS_KEY_ID,
+    APP_AWS_SECRET_ACCESS_KEY: process.env.APP_AWS_SECRET_ACCESS_KEY,
+  },
 };
 
 module.exports = nextConfig;
